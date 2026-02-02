8b76b220a74255d66f8093328a16e6b0f637abc7
Vittorio-415 <1826332474@qq.com>
2026-02-02 14:55:57 +0800

    add more case

--- src/config_path.py
+++ src/config_path.py
@@ -23,8 +23,9 @@ log_with_time(f"config_file_path: {config_file_path}")
 config = load_config(config_file_path)
 
 # Set variables from config
-case_name = config_file_path.split('/')[-1].split('.')[0]
+case_name = os.path.splitext(os.path.basename(config_file_path))[0]
 description = config.get('description', '')
+postProcess_description = config.get('postProcess_description', '')
 mesh_path = config.get('mesh_path', '')
 runfile_path = config.get('runfile_path', '')
 input_file_list = config.get('input_file_list', '')
@@ -34,9 +35,11 @@ temperature = config.get('temperature', 0.7)
 run_times = config.get('run_times', 1)
 MetaGPT_PATH = config.get('MetaGPT_PATH', '')
 model = config.get('model', '')
+openfoam_llm = config.get('openfoam_llm', '')
 openfoam_llm_base_url = config.get('openfoam_llm_base_url', '')
-Run_PATH = f'{Base_PATH}/run'  # Modify to the actual path
+Run_PATH = f'{Base_PATH}/run/' + config.get('run_path', '')  # Modify to the actual path
 should_stop = False
+postprocess_should_stop = False
 status = ''
 
 writter_prompt = ''
@@ -48,6 +51,10 @@ os.environ["PROXY"] = config.get("PROXY", "")
 os.environ["BASE_URL"] = config.get("BASE_URL", "")
 os.environ["http_proxy"] = "http://localhost:7890"
 os.environ["https_proxy"] = "http://localhost:7890"
+# 用于后处理LLM的环境变量
+os.environ["OPENAI_API_KEY"] = config.get("OPENAI_API_KEY", "")
+os.environ["OPENAI_MODEL"] = config.get("OPENAI_MODEL", "")
+os.environ["OPENAI_BASE_URL"] = config.get("OPENAI_BASE_URL", "")
 
 # Add MetaGPT_PATH to sys.path
 sys.path.append(MetaGPT_PATH)
